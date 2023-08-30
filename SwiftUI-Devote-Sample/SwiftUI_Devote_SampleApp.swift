8847333dcb4fdb388206a30836b2a1dabe4a7e59
ekayaint <ekayaint@gmail.com>
2023-08-30 18:52:38 +0300

    Initial Commit

---
+++ SwiftUI-Devote-Sample/SwiftUI_Devote_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Devote_SampleApp.swift
+//  SwiftUI-Devote-Sample
+//
+//  Created by ekayaint on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Devote_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
